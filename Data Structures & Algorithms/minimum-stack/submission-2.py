5340df66cf6149bd5eb00fc782294177f5f7cfe9
Akshata Ramgopal <148430275+Akshataram@users.noreply.github.com>
2026-05-14 23:10:30 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,12 @@
+class MinStack:
+
+    def __init__(self):
+        self.s1=[]
+    def push(self, val: int) -> None:
+        self.s1.append(val)
+    def pop(self) -> None:
+        return self.s1.pop()
+    def top(self) -> int:
+        return self.s1[-1]
+    def getMin(self) -> int:
+        return min(self.s1)
